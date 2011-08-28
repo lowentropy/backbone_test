98018accbaa016cf7e0022d47a7fc9b8ad1c7ac1
Nathan Matthews <lowentropy@gmail.com>
2011-08-28 16:30:05 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneTest::Application.config.secret_token = '45d4bd9d70de5db3457d3efa4fdc0764b7df34a3f71a414fffc48c12d0eab639b8518c7656a502161d62f5d2a4875e3d8267f7ad82bc725b84205db7150a8187'
